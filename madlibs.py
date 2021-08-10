0811e45f626925a9bb616332613fb00e941a1dbd
georgegeiwitzcesana <georgegeiwitzcesana@gmail.com>
2021-08-10 09:30:23 -0700

    first commit

---
+++ madlibs.py
@@ -0,0 +1,28 @@
+import sys
+
+def get_word():
+
+return input('Enter a word:')
+
+def parse_args(args):
+
+return args
+
+for arg in args[1:]:
+
+ret.append(arg)
+
+
+
+args = parse_args(sys.argv)
+
+for index in range(len(args)):
+        
+    if args[index] == '_':
+
+        argv[index] = get word()
+
+result= '!'.join(args[1:]
+
+print(result)
+print(sys.argv) 
