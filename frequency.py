b4659f13138eeb273ca95e68ed1b9d5307e7786d
georgegeiwitzcesana <georgegeiwitzcesana@gmail.com>
2021-08-11 09:15:33 -0700

    cleaning data

--- frequency.py
+++ frequency.py
@@ -1,16 +1,53 @@
-import sys
+1mport sys
 
-text = ' '.join(sys.argv)[1:])
+ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+
+args = sys.argv[1:]
+
+for arg in args:
+
+    print(arg)
+
+text = 'spam '.join(args)
+
+print(text)
+
+lowercase_letter = text[0].lower()
+
+print(lowercase_letter)
+
+lowercase_text = [letter.lower()for letter in text]
+lowercase_text = text.lower()
+print(lowercase_text)
+
+lowercase_text = [letter if letter in ALPHABET else '' for letter in lowercase_text]
+
+print(lowercase_letter)
+lowercase_text = ''.join(lowercase_text)
+
 frequencies = {}
-
-for letter in text:
+for letter in ALPHABET:
+    frequencies[letter] = 0
+for letter in lowercase_text:
 
     if letter not in frequencies:
 
-    frequencies[letter] = 1
+        frequencies[letter] = 1
+    
+    else:
 
-else:
-
-frequencies[letter] += 1
+        frequencies[letter] += 1
 
 print(frequencies)
+
+for letter in ALPHABET:
+
+    count = frequencies[letter]
+
+    print(letter, end=' ')
+
+    for c in range(count):
+
+        print('|', end='')
+
+    print()
