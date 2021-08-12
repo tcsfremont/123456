75a52abdf6d05eda2cfd7df4e4f9e942c033658c
georgegeiwitzcesana <georgegeiwitzcesana@gmail.com>
2021-08-12 13:11:04 -0700

    encipher

--- encipher.py
+++ encipher.py
@@ -1,11 +1,11 @@
 from sys import argv as arguments
 
 ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-KEY = 'DBVYTIZCXHOPJKLEWUFGAQSRCK':
+KEY = 'DBVYTIZCXHOPJKLEWUFGAQSRCK'
 
-if len(arguments) != 4:
+if len(arguments) != 3:
 
-    print("3 arguments required!")
+    print("2 arguments required!")
     exit()
 
 from_filename = arguments[1]
@@ -17,21 +17,21 @@ print("output:" + to_filename)
 from_file = open(from_filename, 'r', encoding='utf8')
 to_file = open(to_filename, 'w', encoding='utf8')
 
-contents = from_file.read()
+contents = list(from_file.read())
 
 for index in range(len(contents)):
 
     letter = contents[index]
     order = ord(letter) - ord('A')
-
-    if order < 0 or order .= len(KEY):
-
-    continue
+   
+    if order < 0 or order >= len(KEY):
+ 
+        continue
 
     cipher_letter = KEY[order]
     contents[index] = cipher_letter
 
-to_file.write(contents)
+to_file.write(''.join(contents))
 
 from_file.close()
 to_file.close()
